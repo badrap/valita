43182bffce32f6c6dc31c053bc0900c5462034bc
Joachim Viide <jviide@iki.fi>
2021-02-26 21:11:51 +0000

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,275 @@
+function isObject(v: unknown): v is Record<string, unknown> {
+  return typeof v === "object" && v !== null && !Array.isArray(v);
+}
+
+type PrettifyObjectType<V> = Extract<{ [K in keyof V]: V[K] }, unknown>;
+
+type ErrorContext = Readonly<
+  | {
+      ok: false;
+      type: "path";
+      value: string | number;
+      current: ErrorContext;
+      next?: ErrorContext;
+    }
+  | {
+      ok: false;
+      type: "error";
+      error: string;
+    }
+>;
+type Ok<T> =
+  | true
+  | Readonly<{
+      ok: true;
+      value: T;
+    }>;
+type Result<T> = Ok<T> | ErrorContext;
+
+function ok<T>(v: T): { ok: true; value: T } {
+  return { ok: true, value: v };
+}
+
+function err(error: string): ErrorContext {
+  return { ok: false, type: "error", error };
+}
+
+type Path = (string | number)[];
+
+type Err = { path: Path; message: string };
+
+function flattenError(ctx: ErrorContext): Err[] {
+  const issues: Err[] = [];
+  const path: Path = [];
+  function traverse(ctx: ErrorContext): void {
+    if (ctx.type === "error") {
+      issues.push({
+        path: path.slice(),
+        message: ctx.error,
+      });
+    } else {
+      if (ctx.next) {
+        traverse(ctx.next);
+      }
+      path.push(ctx.value);
+      traverse(ctx.current);
+      path.pop();
+    }
+  }
+  traverse(ctx);
+  return issues;
+}
+
+class Vx<T> {
+  constructor(private readonly _genFunc: () => (v: unknown) => Result<T>) {}
+
+  get func(): (v: unknown) => Result<T> {
+    const f = this._genFunc();
+    Object.defineProperty(this, "func", {
+      value: f,
+      writable: false,
+    });
+    return f;
+  }
+
+  transform<O>(func: (v: T) => Result<O>): Vx<O> {
+    const f = this.func;
+    return new Vx(() => (v) => {
+      const r = f(v);
+      if (r !== true && !r.ok) {
+        return r;
+      }
+      return func(r === true ? (v as T) : r.value);
+    });
+  }
+  parse(v: unknown): T {
+    const r = this.func(v);
+    if (r === true) {
+      return v as T;
+    } else if (r.ok) {
+      return r.value;
+    } else {
+      console.log(flattenError(r));
+      throw new Error();
+    }
+  }
+  optional(): Vx<T | undefined> {
+    const f = this.func;
+    return new Vx(() => (v) => {
+      return v === undefined ? true : f(v);
+    });
+  }
+}
+
+class VxObj<
+  T extends Record<string, unknown>,
+  Mode extends "passthrough" | "strict" | "strip" | "catchall",
+  Catchall extends Mode extends "catchall" ? unknown : undefined,
+  O = PrettifyObjectType<
+    T &
+      (Mode extends "passthrough" ? { [K: string]: unknown } : unknown) &
+      (Mode extends "catchall" ? { [K: string]: Catchall } : unknown)
+  >
+> extends Vx<O> {
+  private readonly _shape: { [K in keyof T]: Vx<T[K]> };
+  private readonly _mode: Mode;
+  private readonly _catchall?: Vx<Catchall>;
+
+  constructor(
+    shape: { [K in keyof T]: Vx<T[K]> },
+    mode: Mode,
+    catchall?: Vx<Catchall>
+  ) {
+    super(() => {
+      const obj = this._shape;
+
+      const keys: string[] = [];
+      const vals: ((v: unknown) => Result<unknown>)[] = [];
+      for (const k in obj) {
+        keys.push(k);
+        vals.push((obj as any)[k].func);
+      }
+      const l = keys.length;
+      const q = Object.create(null);
+      keys.forEach((k, i) => {
+        q[k] = true;
+      });
+
+      const catchall = this._catchall?.func;
+      const strict = this._mode === "strict";
+      const strip = this._mode === "strip";
+      const passthrough = this._mode === "passthrough";
+
+      const template = {} as Record<string, unknown>;
+      keys.forEach((key) => {
+        template[key] = undefined;
+      });
+
+      return (v) => {
+        if (!isObject(v)) {
+          return err("expected an object");
+        }
+        let ctx: ErrorContext | undefined = undefined;
+        let output = v as Record<string, unknown>;
+        let tpl = strict || strip ? template : v;
+        if (!passthrough) {
+          for (const k in v) {
+            if (!q[k]) {
+              if (strict) {
+                return err(`unexpected key ${JSON.stringify(k)}`);
+              } else if (strip) {
+                output = { ...tpl };
+                break;
+              } else if (catchall) {
+                const r = catchall(v[k]);
+                if (r === true) {
+                  // pass
+                } else if (!r.ok) {
+                  ctx = {
+                    ok: false,
+                    type: "path",
+                    value: k,
+                    current: r,
+                    next: ctx,
+                  };
+                } else {
+                  if (output === v) {
+                    output = { ...tpl };
+                  }
+                  output[k] = r.value;
+                }
+              }
+            }
+          }
+        }
+        for (let i = 0; i < l; i++) {
+          const r = vals[i](v[keys[i]]);
+          if (r === true) {
+            // pass
+          } else if (r.ok) {
+            if (output === v) {
+              output = { ...tpl };
+            }
+            output[keys[i]] = r.value;
+          } else {
+            ctx = {
+              ok: false,
+              type: "path",
+              value: keys[i],
+              current: r,
+              next: ctx,
+            };
+          }
+        }
+        if (ctx) {
+          return ctx;
+        }
+        return v === output ? true : { ok: true, value: output as O };
+      };
+    });
+
+    this._shape = shape;
+    this._mode = mode;
+    this._catchall = catchall;
+  }
+  passthrough(): VxObj<T, "passthrough", undefined> {
+    return new VxObj(this._shape, "passthrough", undefined);
+  }
+  strict(): VxObj<T, "strict", undefined> {
+    return new VxObj(this._shape, "strict", undefined);
+  }
+  strip(): VxObj<T, "strip", undefined> {
+    return new VxObj(this._shape, "strip", undefined);
+  }
+  catchall<X>(vx: Vx<X>): VxObj<T, "catchall", X> {
+    return new VxObj(this._shape, "catchall", vx);
+  }
+}
+
+function number() {
+  const e = err("expected a number");
+  return new Vx<number>(() => (v) => (typeof v === "number" ? true : e));
+}
+function string() {
+  const e = err("expected a string");
+  return new Vx<string>(() => (v) => (typeof v === "string" ? true : e));
+}
+function boolean() {
+  const e = err("expected a boolean");
+  return new Vx<string>(() => (v) => (typeof v === "boolean" ? true : e));
+}
+function undefined_() {
+  const e = err("expected undefined");
+  return new Vx<undefined>(() => (v) => (v === undefined ? true : e));
+}
+function null_() {
+  const e = err("expected null");
+  return new Vx<null>(() => (v) => (v === null ? true : e));
+}
+function object<T extends Record<string, unknown>>(
+  obj: { [K in keyof T]: Vx<T[K]> }
+) {
+  type Optionals = {
+    [K in keyof T]: undefined extends T[K] ? K : never;
+  }[keyof T];
+
+  return new VxObj<
+    {
+      [K in Optionals]?: T[K];
+    } &
+      {
+        [K in Exclude<keyof T, Optionals>]: T[K];
+      },
+    "strict",
+    undefined
+  >(obj, "strict", undefined);
+}
+
+export {
+  number,
+  string,
+  boolean,
+  null_ as null,
+  undefined_ as undefined,
+  object,
+};
